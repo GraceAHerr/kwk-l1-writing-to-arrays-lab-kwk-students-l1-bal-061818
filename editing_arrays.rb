eab59cf9ce86830ba07a967cb162b92f3e55924e
GraceAHerr <mazyg321@gmail.com>
2018-06-20 17:29:23 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -4,7 +4,8 @@ rainbow_colors = [
   "light_cyan"
 ]
 
-def change_rainbow_colors(rainbow_colors)
+def change_rainbow_colors
+
   puts rainbow_colors[0]="red"
   puts rainbow_colors[1]="light_red"
   puts rainbow_colors[2]="light_yellow"
